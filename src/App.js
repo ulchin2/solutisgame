e2a15fc9c8f3715713bef86115eb106c19d73807
Marcus Vincius S Silva <deadinsidehlp@gmail.com>
2023-03-19 03:13:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import sollogo from './assets/logo.jpg'
+import {validateEmail, validatePassword} from './utils/regex'
+
+import './styles.css'
 
 function App() {
+
+    const[email, setEmail] = useState("")
+    const[password, setPassword] = useState("")
+
+
+    const[emailErr, setEmailErr] = useState(false);
+    const[passwordErr, setPasswordErr] = useState(false);
+
+    const validate = () => {
+      if(!validateEmail.test(email)){
+        setEmailErr(true)
+      }else{
+        setEmailErr(false)
+      }
+
+      if(!validatePassword.test(password)){
+        setPasswordErr(true)
+      }else{
+        setPasswordErr(false)
+      }
+      
+    };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container">
+    <div className="container-login">
+      <div className="wrap-login">
+        <form className="login-form">
+          <span className="login-form-title">Bem Vindo!</span>
+          <span className="login-form-title">
+            <img src={sollogo} alt="Solutis" />
+          </span>
+
+          <div className="wrap-input">
+            <input 
+              className={email !=="" ? 'has-val input' : 'input'}
+              type="email" 
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <span className="focus-input" data-placeholder= "E-mail"></span>
+            {emailErr && <p>Por Favor digite um email valido!</p>}
+          </div>
+
+          <div className="wrap-input">
+            <input 
+              className={password !=="" ? 'has-val input' : 'input'} 
+              type="password" 
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <span className="focus-input" data-placeholder= "Senha"></span>
+            {passwordErr && <p>Por Favor digite uma senha mais segura!</p>}
+          </div>
+
+          <div className="container-login-form-btn">
+            <button name='login' disabled="" type="button" onClick={validate} className="login-form-btn">Login</button>
+          </div>
+
+          <div className="text-center">
+            <span className="txt1">Não possui conta?</span>
+
+            <a className="txt2" href="#">Criar Conta</a>
+          </div>
+
+        </form>
+      </div>
     </div>
+   </div>
   );
 }
 
---
+++ src/utils/regex.js
@@ -0,0 +1,5 @@
+export const validateEmail = new RegExp(
+    "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+);
+
+export const validatePassword = new RegExp("^(?=.*\d.*\d)(?=.*[A-Z])(?=.*[@#$%\^&+=]).{8,}$");
